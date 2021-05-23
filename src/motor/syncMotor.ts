93995bb953f0a7ffc795c7894b140927b295cc63
kaneko656  <nakano656sho@gmail.com>
2021-05-23 16:30:35 +0900

    整数への変換処理

--- src/motor/syncMotor.ts
+++ src/motor/syncMotor.ts
@@ -8,7 +8,7 @@ export const createSyncMotor = () => {
   let socket: WebSocketClient | undefined
   let isInitialized = false
   const send = async (api: { uri: string; method: string }, data: any) => {
-    if(!isInitialized) throw Error(NOT_INITIALIZED)
+    if (!isInitialized) throw Error(NOT_INITIALIZED)
     const uri = api.uri
     if (socket) {
       return socket.send(uri, api.method, data).catch(async (error) => {
@@ -27,36 +27,81 @@ export const createSyncMotor = () => {
     },
 
     stop: async (prop?, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
       }
       return send(API.stop, prop)
     },
 
     lock: async (prop?, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
       }
       return send(API.lock, prop)
     },
 
     rotate: async (prop, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
       }
+      if (Array.isArray(prop)) {
+        prop = prop.map((p) =>
+          p
+            ? {
+                ...p,
+                speed: p.speed ? Math.round(p.speed) : p.speed,
+              }
+            : p
+        )
+      } else {
+        if(prop) prop.speed = prop.speed ? Math.round(prop.speed) : prop.speed
+      }
+
       return send(API.rotate, prop)
     },
 
     rotateBy: async (prop, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
+      }
+      if (Array.isArray(prop)) {
+        prop = prop.map((p) =>
+          p
+            ? {
+                ...p,
+                speed: p.speed ? Math.round(p.speed) : p.speed,
+                degree: p.degree ? Math.round(p.degree) : p.degree,
+              }
+            : p
+        )
+      } else {
+        if(prop) {
+          prop.speed = prop.speed ? Math.round(prop.speed) : prop.speed
+          prop.degree = prop.degree ? Math.round(prop.degree) : prop.degree
+        }
       }
       return send(API.rotateBy, prop)
     },
 
     rotateTo: async (prop, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
+      }
+      if (Array.isArray(prop)) {
+        prop = prop.map((p) =>
+          p
+            ? {
+                ...p,
+                speed: p.speed ? Math.round(p.speed) : p.speed,
+                degree: p.degree ? Math.round(p.degree) : p.degree,
+              }
+            : p
+        )
+      } else {
+        if(prop) {
+          prop.speed = prop.speed ? Math.round(prop.speed) : prop.speed
+          prop.degree = prop.degree ? Math.round(prop.degree) : prop.degree
+        }
       }
       return send(API.rotateTo, prop)
     },
@@ -66,8 +111,20 @@ export const createSyncMotor = () => {
     },
 
     resetRotation: async (prop, prop2?) => {
-      if(prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
+      if (prop !== undefined && prop2 !== undefined && !Array.isArray(prop)) {
         prop = [prop, prop2]
+      }
+      if (Array.isArray(prop)) {
+        prop = prop.map((p) =>
+          p
+            ? {
+                ...p,
+                speed: p.offset ? Math.round(p.offset) : p.offset,
+              }
+            : p
+        )
+      } else {
+        if(prop) prop.offset = prop.offset ? Math.round(prop.offset) : prop.offset
       }
       return send(API.resetRotation, prop)
     },
